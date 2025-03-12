01b7689321831c6c5db4d7a6fc7091cc4ee1748b
Frankincense <frankincensewelsey@gmail.com>
2025-03-12 08:26:33 +0300

    Array method find method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(arr) {
+    const winYear = arr.find(function(obj){
+        if (obj.result === "W") {
+            return obj.year
+        }
+        else{
+            return
+        }
+    })
+    if (winYear) {
+        return winYear.year
+    }
+    return winYear
+}
+
+console.log(superbowlWin(record))
